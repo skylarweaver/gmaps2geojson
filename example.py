464cb072c1a2368363ac7d21132e6a98a55f3e25
Skylar Weaver <sweaver@deloitte.com>
2019-04-28 16:40:09 -0400

    Gen coords from array of locations

--- example.py
+++ example.py
@@ -1,6 +1,12 @@
 import gmaps2geojson
 
+# # Uncomment below to query each direction set one by one:
+# writer = gmaps2geojson.Writer()
+# writer.query("2131 7th Ave, Seattle, WA 98121", "900 Poplar Pl S, Seattle, WA 98144")
+# writer.query("900 Poplar Pl S, Seattle, WA 98144", "219 Broadway E, Seattle, WA 98102")
+# writer.save("example.geojson")
+
+# Query array of locations:
 writer = gmaps2geojson.Writer()
-writer.query("2131 7th Ave, Seattle, WA 98121", "900 Poplar Pl S, Seattle, WA 98144")
-writer.query("900 Poplar Pl S, Seattle, WA 98144", "219 Broadway E, Seattle, WA 98102")
+directions = writer.query_array([["San Carlos, Antioquia, Colombia", "Villavicencio, Colombia"],["Villavicencio, Colombia","Cali, Colombia"],["Cali, Colombia","Quito, Ecuador"] ])
 writer.save("example.geojson")
--- gmaps2geojson/writer.py
+++ gmaps2geojson/writer.py
@@ -23,19 +23,32 @@ class Writer:
 
         default_name = "{0} to {1}".format(src, dest)
 
-        self.features.append({
-            "type": "Feature",
-            "properties": {
-                "name": custom_label or default_name
-            },
-            "geometry": {
-                "type": "MultiLineString",
-                "coordinates": [coords_list]
-            }
-        })
+        self.features = self.features + (coords_list)
         return [[lat, lon] for lat, lon in coords]
 
+    # Accepts array for [src, dest] string place names
+    def query_array(self, src_dest_array, custom_label = None):
+        # src_dest_array is array of [src, dest] string place names
+        for [src, dest] in src_dest_array:
+            request_url = 'https://maps.googleapis.com/maps/api/directions/json?origin="{0}"&destination="{1}"&key={2}'.format(src, dest, os.environ["GMAPS_KEY"])
+            try:
+                r = requests.get(request_url)
+                results = r.json()
+                route = results['routes'][0]['overview_polyline']['points']
+                coords = polyline.decode(route)
+            except Exception as e:
+                print("Error querying for directions for {0} tp {1} -- {2}".format(src, dest, str(e)))
+                return
+
+            # reverse order to comply with geojson spec
+            coords_list = [[lon, lat] for lat, lon in coords]
+
+            default_name = "{0} to {1}".format(src, dest)
+
+            self.features = self.features + (coords_list)
+        return self.features
+
+    # Outputs array of [long, lat] geojson coordinates
     def save(self, filename):
-        geojson = {"type": "FeatureCollection", "features": self.features}
         with open(filename, "w") as out:
-            json.dump(geojson, out)
+            json.dump(self.features, out)
